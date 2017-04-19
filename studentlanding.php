0f758d318894b80919177db119a3eb3799e7e163
eniol <a@a.com>
2017-04-19 21:54:52 +0100

    viewcoursework file created

--- studentlanding.php
+++ studentlanding.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['userSession'])||$_SESSION['active'] == false){
         <br class="breaks">
         <div class="col-md-4 mydash">
             <i class="fa fa-book fafont"></i>
-            <a class="dashlink" href="../../Desktop/MustaphAkinsanmi/myRGU2016/CMM007IntranetSystemDevelopment/courseworkPart2/viewcoursework.php">Course Works</a>
+            <a class="dashlink" href="viewcoursework.php">Course Works</a>
         </div>
         <div class="col-md-4 mydash">
             <i class="fa fa-file-text fafont"></i>
