6a47fb8c2625d9dbb06a2f7b8777f028693ff7b3
rgu1613692 <m.akinsanmi@rgu.ac.uk>
2017-04-20 14:52:11 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -67,7 +67,7 @@ $mail = new PHPMailer;
                     $mail->Password = 'Trialpeerassessment)(';
 
 
-                    $mail->setFrom('offorma@gmail.com', 'Warranty Store');
+                    $mail->setFrom('trialpeerassessment@gmail.com', 'Peer Assessment');
                     $mail->addAddress("$email", "$uname");
                     $mail->Subject = 'Signup | Verification';
                     $mail->Body = 'Thanks for signing up!
