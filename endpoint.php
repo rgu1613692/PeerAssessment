be0f239fbc223d8523419cf6ccfd6c1750c3d996
rgu1613692 <m.akinsanmi@rgua.ac.uk>
2017-05-04 14:43:39 +0100

    phpmailerfile created

--- endpoint.php
+++ endpoint.php
@@ -37,7 +37,7 @@ if ($method=='GET') {
         header('Content-Type: application/text');
         //going to this url will create a new course in the db http://myassessment.azurewebsites.net/endpoint.php/course/coursecode/coursename
         if (($requ->length ==3)) {
-            if ($requ[0]==course){
+            if (true){
                 $query ="INSERT INTO course(courseid,code,coursename)VALUES('','$requ[1]','$requ[2]')";
                 $result = $link->query($query);
                 if ($result) {
