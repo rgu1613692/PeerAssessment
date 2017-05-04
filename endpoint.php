612a7c16a08463358968061b59240a27638573b3
rgu1613692 <m.akinsanmi@rgua.ac.uk>
2017-05-04 14:39:56 +0100

    phpmailerfile created

--- endpoint.php
+++ endpoint.php
@@ -39,7 +39,7 @@ if ($method=='GET') {
         if (($requ->length ==3)) {
             if ($requ[0]==course){
                 $query ="INSERT INTO course(code,coursename)VALUES( '$requ[1]', '$requ[2]')";
-             $result = $link->query($query);
+                $result = $link->query($query);
                 if ($result) {
                     header("HTTP/1.1 200 OK");
                     echo 'success';
