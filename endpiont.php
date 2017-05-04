fa6cfd4d264939e29030cb9089e20bfc6aa89c7b
rgu1613692 <m.akinsanmi@rgua.ac.uk>
2017-05-04 13:36:00 +0100

    phpmailerfile created

--- endpiont.php
+++ endpiont.php
@@ -8,10 +8,10 @@
 
 
 
-var_dump($_SERVER['PATH_INFO']);
-$url = explode ("/", substr($_SERVER['PATH_INFO'], 1));
-
-print_r($url);
+$request_verb = $_SERVER['REQUEST_METHOD'];
+$requ = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
+$requ = array_map('strtolower', $requ);
+echo $requ;
 
 header('Content-Type: application/json');
 include('db.php');
