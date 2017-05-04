6d458348ad6b62106321da0c8dfb2105366e50e4
rgu1613692 <m.akinsanmi@rgua.ac.uk>
2017-05-04 15:01:43 +0100

    phpmailerfile created

--- endpoint.php
+++ endpoint.php
@@ -44,85 +44,32 @@ if ($method=='POST') {
                     header("HTTP/1.1 200 OK");
                     echo 'success';
                     }else{
-                    echo "something went wrong";
+                    echo "something went wrong make sure your content is unique";
+                    }
                 }
-                 }
 
             }
         elseif (sizeof($requ) !=3){
             echo 'Please provide the required parameters in the URL in the format /course/coursecode/coursename';
         }
-}/* else {
-            $jason = array("message" => "No Records Found for Your Query");
-            echo $jason;
-        }
-                } elseif (($request == "users")) {
-                    //going to this url will return all users in the db http://myassessment.azurewebsites.net/endpoint.php?query=users
-                    $query = "SELECT userid, username, email FROM users";
-                    $result = $link->query($query);
-                    if ($result->num_rows > 0) {
-                        $arrayIndex = 0;
-                        while ($single = $result->fetch_assoc()) {
-                            $dataArray[$arrayIndex] = $single;
-                            $arrayIndex++;
-                        }
-                        $jason = json_encode($dataArray);
-                        $jason = indent($jason);
-                        echo $jason;
-                    } else {
-                        $jason = array("message" => "No Records Found for Your Query");
-                        echo $jason;
-                    }
-                } elseif (($request == "task")) {
-                    //going to this url will return all tasks in the db http://myassessment.azurewebsites.net/endpoint.php?query=task
-                    $query = "SELECT * FROM task";
-                    $result = $link->query($query);
-                    if ($result->num_rows > 0) {
-                        $arrayIndex = 0;
-                        while ($single = $result->fetch_assoc()) {
-                            $dataArray[$arrayIndex] = $single;
-                            $arrayIndex++;
-                        }
-                        $jason = json_encode($dataArray);
-                        $jason = indent($jason);
-                        echo $jason;
-                    } else {
-                        $jason = array("message" => "No Records Found for Your Query");
-                        echo $jason;
-                    }
-                }
+}
+if ($method=='PUT') {
+    header('Content-Type: application/text');
+    //going to this url will create a new course in the db http://myassessment.azurewebsites.net/endpoint.php/course/courseid/coursecode/coursename
+    if (sizeof($requ) == 4) {
+        if ($requ[0] == "course"){
+            $query ="UPDATE course SET coursename='$requ[3]',code='$requ[2]' WHERE courseid='$requ[1]'";
+            $result = $link->query($query);
+            if ($result) {
+                header("HTTP/1.1 200 OK");
+                echo 'success';
+            }else{
+                echo "something went wrong make sure your content is unique";
             }
-            function indent($json)
-            {
-                $quo = '';
-                $pos = 0;
-                $strLen = strlen($json);
-                $indentStr = '  ';
-                $newLine = "\n";
-                $prevChar = '';
-                $outOfQuotes = true;
-                for ($i = 0; $i <= $strLen; $i++) {
-                    $char = substr($json, $i, 1);
-                    if ($char == '"' && $prevChar != '\\') {
-                        $outOfQuotes = !$outOfQuotes;
-                    } else if (($char == '}' || $char == ']') && $outOfQuotes) {
-                        $quo .= $newLine;
-                        $pos--;
-                        for ($j = 0; $j < $pos; $j++) {
-                            $quo .= $indentStr;
-                        }
-                    }
-                    $quo .= $char;
-                    if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-                        $quo .= $newLine;
-                        if ($char == '{' || $char == '[') {
-                            $pos++;
-                        }
-                        for ($j = 0; $j < $pos; $j++) {
-                            $quo .= $indentStr;
-                        }
-                    }
-                    $prevChar = $char;
-                }
-                return $quo;
-            }*/
+        }
+
+    }
+    elseif (sizeof($requ) !=3){
+        echo 'Please provide the required parameters in the URL in the format /course/coursecode/coursename';
+    }
+}
